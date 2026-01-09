030c3c0852c396f2b105388d32b259e3e311eaeb
Your Name <you@example.com>
2026-01-09 11:01:10 +0700

    slot2/khaibaosothuc.c

---
+++ slot2/khaibaosothuc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // khai bao 1 bien so thuc
+    float a;
+
+    // khoi tao gia tri bat ky
+    a = 3.14;
+
+    // in ra man hinh
+    printf("Gia tri cua a la: %.2f\n", a);
+
+    return 0;
+}
