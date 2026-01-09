05716ee74ff225d1600fcde853e8f0699c68fdda
Your Name <you@example.com>
2026-01-09 10:40:12 +0700

    anc

---
+++ slot2/baitap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // khai bao 1 bien so nguyen
+    int a;
+    // khoi tao gia tri bat ky
+    a = 10;
+    // in ra man hinh
+    printf("Gia tri cua a la: %d\n", a);
+    return 0;
+}
