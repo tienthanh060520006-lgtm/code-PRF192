842fb3789342879cb5733376b7d9a8c001857c69
Your Name <you@example.com>
2026-01-21 09:46:18 +0700

    slot5

--- slot5/ex4.c
+++ slot5/ex4.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int money;
     int sokwh;
-    printf("Nhap so kwh: ");      
-    scanf("%d", &sokwh);         
-    if (sokwh < 100) {
+    int money;
+
+    printf("Nhap so kwh: ");
+    scanf("%d", &sokwh);
+
+    if (sokwh <= 100) {
         money = sokwh * 950;
     } 
-    else if (sokwh > 100 && sokwh < 150) {
-        money = sokwh * 1250;
+    else if (sokwh <= 150) {
+        money = (100 * 950) + (sokwh - 100) * 1250;
     } 
-    else if (sokwh > 150 && sokwh < 200) {
-        money = sokwh * 1350;
+    else if (sokwh <= 200) {
+        money = (100 * 950) + (50 * 1250) + (sokwh - 150) * 1350;
     } 
     else {
-        money = sokwh * 1550;
+        money = (100 * 950) + (50 * 1250) + (50 * 1350) + (sokwh - 200) * 1550;
     }
-    printf("So tien dien la: %d", money);
+
+    printf("Tong tien dien phai tra la: %d VND\n", money);
+
     return 0;
 }
