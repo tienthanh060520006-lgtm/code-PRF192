a5fbf68806e9ae3538f9c27903d24ce03bda6488
Your Name <you@example.com>
2026-02-05 22:49:54 +0700

    Workshop/Workshop2.c

---
+++ Workshops/Workshop2/Ex02'.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+float n;
+int count = 1;
+		while(1) {
+		printf("Nhap so thu %d: ", count);
+		while(scanf("%f", &n) != 1 || n <= 0) {
+			printf("Nhap lai so thu %d: ", count);
+			fflush(stdin);
+		}
+		float kq = sqrt(n);
+		printf("Can bac hai la: %.2f\n", kq);
+		count ++;
+		if(count == 6) break;
+	}
+	printf("Da xong! Xuat sac!");
+	return 0;
+}
+	
