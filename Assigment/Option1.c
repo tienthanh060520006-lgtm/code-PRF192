9b4c7d45ba3f3a623ec5c63cdd47bfbaadd229fb
Your Name <you@example.com>
2026-03-26 21:45:54 +0700

    update

--- Assigment/Option1.c
+++ Assigment/Option1.c
@@ -9,21 +9,31 @@ typedef struct {
     int stock;
 } Item;
 
+void clearBuffer();
+void saveItems(const char *file, int count);
+void addMoreItems(const char *file, int count);
+void displayItems(const char *file);
+void updateItem(const char *file);
 
 int main() {
     const char *filePath = "products.bin";
     int option, amount;
 
     do {
-        printf("\n=== Inventory Management ===\n");
+        printf("\n=== INVENTORY MANAGEMENT ===\n");
         printf("1. Write New Items\n");
         printf("2. Append Items\n");
         printf("3. Show All Items\n");
         printf("4. Edit Item\n");
         printf("5. Quit\n");
-
         printf("Select an option: ");
-        scanf("%d", &option);
+
+        if (scanf("%d", &option) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            clearBuffer();
+            option = 0;
+            continue;
+        }
 
         switch (option) {
             case 1:
@@ -31,39 +41,37 @@ int main() {
                 scanf("%d", &amount);
                 saveItems(filePath, amount);
                 break;
-
             case 2:
                 printf("How many items to append: ");
                 scanf("%d", &amount);
                 addMoreItems(filePath, amount);
                 break;
-
             case 3:
                 displayItems(filePath);
                 break;
-
             case 4:
                 updateItem(filePath);
                 break;
-
             case 5:
-                printf("Exiting.\n");
+                printf("Exiting program.\n");
                 break;
-
             default:
-                printf("Invalid option. Try again.\n");
+                printf("Option not available.\n");
         }
-
     } while (option != 5);
 
     return 0;
 }
 
-// Write (overwrite) items
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void saveItems(const char *file, int count) {
     FILE *fp = fopen(file, "wb");
     if (!fp) {
-        printf("Cannot open file.\n");
+        printf("Error: Could not open file.\n");
         return;
     }
 
@@ -72,27 +80,24 @@ void saveItems(const char *file, int count) {
         printf("\nItem %d\n", i + 1);
         printf("ID: ");
         scanf("%d", &temp.id);
+        clearBuffer();
         printf("Name: ");
-        getchar();
         fgets(temp.name, 50, stdin);
         temp.name[strcspn(temp.name, "\n")] = '\0';
         printf("Price: ");
         scanf("%f", &temp.price);
         printf("Stock: ");
         scanf("%d", &temp.stock);
-
         fwrite(&temp, sizeof(Item), 1, fp);
     }
-
     fclose(fp);
-    printf("Items saved successfully.\n");
+    printf("Successfully saved.\n");
 }
 
-// Append items
 void addMoreItems(const char *file, int count) {
     FILE *fp = fopen(file, "ab");
     if (!fp) {
-        printf("Cannot open file.\n");
+        printf("Error: Could not open file.\n");
         return;
     }
 
@@ -101,86 +106,67 @@ void addMoreItems(const char *file, int count) {
         printf("\nNew Item %d\n", i + 1);
         printf("ID: ");
         scanf("%d", &temp.id);
+        clearBuffer();
         printf("Name: ");
-        getchar();
         fgets(temp.name, 50, stdin);
         temp.name[strcspn(temp.name, "\n")] = '\0';
         printf("Price: ");
         scanf("%f", &temp.price);
         printf("Stock: ");
         scanf("%d", &temp.stock);
-
         fwrite(&temp, sizeof(Item), 1, fp);
     }
-
     fclose(fp);
-    printf("Items appended successfully.\n");
+    printf("Successfully appended.\n");
 }
 
-// Read items
 void displayItems(const char *file) {
     FILE *fp = fopen(file, "rb");
     if (!fp) {
-        printf("File not found.\n");
+        printf("Data file is empty or not found.\n");
         return;
     }
 
     Item temp;
-
-    printf("\n-----------------------------------------------------\n");
-    printf("%-10s %-20s %-10s %-10s\n", "ID", "Name", "Price", "Stock");
+    printf("\n%-10s %-20s %-10s %-10s\n", "ID", "Name", "Price", "Stock");
     printf("-----------------------------------------------------\n");
-
     while (fread(&temp, sizeof(Item), 1, fp)) {
-        printf("%-10d %-20s %-10.2f %-10d\n",
-               temp.id, temp.name, temp.price, temp.stock);
+        printf("%-10d %-20s %-10.2f %-10d\n", temp.id, temp.name, temp.price, temp.stock);
     }
-
-    printf("-----------------------------------------------------\n");
-
     fclose(fp);
 }
 
-// Modify item
 void updateItem(const char *file) {
     FILE *fp = fopen(file, "rb+");
     if (!fp) {
-        printf("File not found.\n");
+        printf("Error: File not found.\n");
         return;
     }
 
-    int searchID;
+    int searchID, found = 0;
     printf("Enter ID to update: ");
     scanf("%d", &searchID);
 
     Item temp;
-    int found = 0;
-
     while (fread(&temp, sizeof(Item), 1, fp)) {
         if (temp.id == searchID) {
             found = 1;
-
+            printf("Found! Enter new details:\n");
+            clearBuffer();
             printf("New Name: ");
-            getchar();
             fgets(temp.name, 50, stdin);
             temp.name[strcspn(temp.name, "\n")] = '\0';
-
             printf("New Price: ");
             scanf("%f", &temp.price);
-
             printf("New Stock: ");
             scanf("%d", &temp.stock);
 
-            fseek(fp, -sizeof(Item), SEEK_CUR);
+            fseek(fp, -(long)sizeof(Item), SEEK_CUR);
             fwrite(&temp, sizeof(Item), 1, fp);
-
-            printf("Item updated successfully.\n");
+            printf("Update complete.\n");
             break;
         }
     }
-
-    if (!found)
-        printf("Item not found.\n");
-
+    if (!found) printf("Item ID %d not found.\n", searchID);
     fclose(fp);
 }
